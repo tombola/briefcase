dd6ab073d0937b6a4e3d0d23c6f5370017b2f761
Tom Readings <digitisatom@gmail.com>
2014-01-21 15:30:10 +0000

    add routes for pages plus controller actions and rspec (capybara) tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,26 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "GET /static_pages" do
-    it "should have the content 'briefcase'" do
+  describe "Home Page" do
+    it "should have the content 'Briefcase'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
         visit '/static_pages/home'
         page.should have_content('Briefcase')
+    end
+  end
+  describe "Help Pages" do
+    it "should have the content 'Help'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+        visit '/static_pages/help'
+        page.should have_content('Help')
+    end
+  end
+  describe "About Pages" do
+
+    it "should have a heading including 'About'" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+        visit '/static_pages/about'
+        page.should have_selector('h1', :text => "About")
     end
   end
 end
