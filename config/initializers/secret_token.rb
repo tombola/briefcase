9ada526bc853f5f52bfb7886802d4ce874b43e1a
Tom Readings <digitisatom@gmail.com>
2014-01-21 11:24:09 +0000

    first commit. Configure gemfile, install rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Briefcase::Application.config.secret_token = '5c1cb9dabde0d559036f5198256ee75c6ee8309960b2d6f95e42fcb9b1482536ea628a5845ef195e13e4fd788cc091666f67c7e637c96a12f769d71bdcf5795c'
